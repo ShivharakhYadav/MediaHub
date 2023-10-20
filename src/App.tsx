59ef6d30b0a2364e6a0b317984b6d58d53afb684
ShivIfour <ifour.shivharakh@gmail.com>
2023-10-20 12:12:17 +0530

    Prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from '../src/pages/Register';
 import React from 'react';
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register />}>
+          {/* <Route */}
+          {/* <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Register.tsx
@@ -0,0 +1,14 @@
+import { Box, Container, TextField } from "@mui/material";
+function Register() {
+    return <>
+        <Container disableGutters>
+            <Box>
+                <TextField />
+                <TextField />
+
+            </Box>
+        </Container>
+    </>
+}
+
+export default Register;
